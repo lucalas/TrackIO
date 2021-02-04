9fcb290ccd6978e1401967f6acb3b1d2b692b708
Luca Taddeo <luca_taddeo@hotmail.it>
2021-02-05 00:18:32 +0100

    Tracked yellow ball and implemented mouse movement;

--- TrackIO.py
+++ TrackIO.py
@@ -1,62 +1,47 @@
 import numpy as np
 import cv2 as cv
-import imutils
-from imutils.video import VideoStream
-import time
-import pyautogui
-#from pymouse import PyMouse
+from pynput.mouse import Controller
 
+lower_yellow = (15, 100, 100)
+upper_yellow = (40, 255, 255)
 
+kernel = np.ones((10,10),np.uint8)
+
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return int(rightMin + (valueScaled * rightSpan))
 
 vs = cv.VideoCapture(2)
+mouse = Controller()
 
-time.sleep(2.0)
-
-debounce = 5
-debounceCount=0
-
-#m = PyMouse()
 while True:
     frame = vs.read()
     frame = frame[1]
-
-    #frame = imutils.resize(frame, width=600)
 
     # Making image smoother
     median = cv.medianBlur(frame,5)
 
     # Filtering image by color
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    lower_yellow = np.array([15, 100, 100])
-    upper_yellow = np.array([40, 255, 255])
     mask = cv.inRange(hsv, lower_yellow, upper_yellow)
 
-    kernel = np.ones((10,10),np.uint8)
     morphed = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel)
     ret,thresh = cv.threshold(morphed,127,255,cv.THRESH_BINARY)
-    contours,hierarchy = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    contours,hierarchy = cv.findContours(morphed, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     if len(contours) > 0:
-        cnt = contours[0]
-
         #usefull in case to look at what we are filtering
-        #res = cv.bitwise_and(frame, frame, mask = morphed)
+        res = cv.bitwise_and(frame, frame, mask = morphed)
 
-        (x,y),radius = cv.minEnclosingCircle(cnt)
-        #m.move(x,y)
-        if (debounceCount % debounce == 0):
-            pyautogui.moveTo(x,y, 0.1)
-            debounceCount = 0
+        (x,y),radius = cv.minEnclosingCircle(contours[0])
 
-        center = (int(x),int(y))
-        radius = int(radius)
-        img = cv.circle(morphed,center,radius,(255, 255, 0),2)
-        #cv.imshow('frame', img)
-        #print(x,y)
-        #time.sleep(0.001)
-        debounceCount = debounceCount + 1
-    #else:
-        #cv.imshow('frame', morphed)
-    
-    if cv.waitKey(10) & 0xFF == ord('q'):
-        cv.destroyAllWindows()
-        break
+        x = translate(x, 0, 600, 0, 2120)
+        y = translate(y, 0, 600, 0, 1280)
+        mouse.position = (x,y)
+        cv.imshow('frame', res)
